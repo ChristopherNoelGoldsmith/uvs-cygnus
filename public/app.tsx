ed4e6d12c1a32c1a74f120eebb2306bd0a43bb48
Christopher Goldsmith <christophergoldsmith101@yahoo.com>
2022-09-30 06:13:55 -0700

    first commit: tsx fully functional

---
+++ public/app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./sass/app.scss";
+
+const App = () => {
+
+	return <p>{"WELCOME TO THE TSX TEMPLATE ENVIRONMENT"}</p>;
+};
+
+export default App;
--- public/index.ts
+++
@@ -1,6 +0,0 @@
-import App from "./app";
-import "./sass/index.scss";
-
-(function () {
-	App();
-})();
